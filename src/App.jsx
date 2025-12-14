ef134e1973258e8cb985311d7bc0efed269f188b
se-u <azrielsebastian@protonmail.ch>
2025-12-14 12:02:42 +0700

    responsive layout and react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
-import SignInPage from "./pages/SignIn";
+import SignInPage from "./pages/signIn";
 import ErrorPage from "./pages/error";
 import DashboardPage from "./pages/dashboard";
 import BalancePage from "./pages/balance";
-import SignUpPage from "./pages/SignUp";
+import SignUpPage from "./pages/signUp";
 
 function App() {
   const myRouter = createBrowserRouter([
